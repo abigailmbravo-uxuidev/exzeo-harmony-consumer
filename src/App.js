6a90e88d8e7c155979227d0eb9cb5a258a5cdf1f
Joe Sutphin <jsutphin@exzeo.com>
2019-08-06 14:56:49 -0400

    heading styles

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
       <header>{/*  header stuff here */}</header>
 
       <div role="main">
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <h4>Heading 4</h4>
+        <p>this is a paragragh</p>
         <nav>
           <ul>
             {/* accordion */}
