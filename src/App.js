2dc63eb76a23afedcd956cac7b9b41d4275906cb
Joe Sutphin <jsutphin@exzeo.com>
2019-08-06 15:31:45 -0400

    styling updates, header styling, logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import logo from './img/TypTap.svg';
+
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
-      <header>{/*  header stuff here */}</header>
+      <header>
+        <a id="logo" className="logo" href="/" data-test="logo">
+          <img src={logo} alt="TypTap Insurance" />
+        </a>
+      </header>
 
       <div role="main">
         <h1>Heading 1</h1>
