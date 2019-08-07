c05f4a6ae60c9a40e5e0d1a4ce81c89c0964df87
Abby Bravo <abravo@fl01mb6280.hci01.local>
2019-08-07 11:47:26 -0400

    navigatoin styling complete

--- src/App.js
+++ src/App.js
@@ -24,9 +24,53 @@ function App() {
           <ul>
             {/* accordion */}
             <li>
+              <a>
+                <h3>Quote</h3>
+                <span>1</span>
+                <p></p>
+              </a>
               <ul>
-                <li>{/*  nav links */}</li>
+                <li>
+                  {/*  nav links */}
+                  <a>
+                    <h3>Address</h3>
+                    <span></span>
+                    <p>
+                      <strong>101 E KENNEDY BLVD</strong>
+                      <br />
+                      Tampa, FL 33602 <br />
+                      Year Built: 2005
+                      <br />
+                      Flood Zone: "A"
+                    </p>
+                  </a>
+                </li>
+                <li>
+                  {/*  nav links */}
+                  <a className="active">
+                    <h3>Underwriting</h3>
+                    <span></span>
+                    <p>You have successfully answered the required questions</p>
+                  </a>
+                </li>
+                <li>
+                  {/*  nav links */}
+                  <a className="disabled">
+                    <h3>Customize Quote</h3>
+                    <span></span>
+                    <p>
+                      Save your quote to retrieve later. You will also be able
+                      to share your new quote here.
+                    </p>
+                  </a>
+                </li>
               </ul>
+            </li>
+            <li>
+              <a className="disabled">
+                <h3>Application</h3>
+                <span>2</span>
+              </a>
             </li>
           </ul>
           {/*  some nav goes here */}
