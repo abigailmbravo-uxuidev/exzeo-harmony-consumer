ab474d6151ae91f8dbde67a9ed11d8c784dbde0d
Abby Bravo <abravo@fl01mb6280.hci01.local>
2019-08-08 09:38:03 -0400

    typography & nav completion

--- src/App.js
+++ src/App.js
@@ -75,14 +75,7 @@ function App() {
           </ul>
           {/*  some nav goes here */}
         </nav>
-        <main role="main">
-          {/*  gandalf will live here */}
-          <h1>Heading 1</h1>
-          <h2>Heading 2</h2>
-          <h3>Heading 3</h3>
-          <h4>Heading 4</h4>
-          <p>this is a paragragh</p>
-        </main>
+        <main role="main">{/*  gandalf will live here */}</main>
       </div>
       <footer>{/*  footer stuff here maybe */}</footer>
     </React.Fragment>
