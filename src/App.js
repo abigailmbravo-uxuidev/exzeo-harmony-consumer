f73c1cd3b4b35633d629b6e776ddefc84b0ef46d
Joe Sutphin <jsutphin@exzeo.com>
2019-08-12 14:29:38 -0400

    add font awesome

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import logo from './img/TypTap.svg';
 
 import './App.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faEnvelope, faKey);
+
 function App() {
   return (
     <React.Fragment>
