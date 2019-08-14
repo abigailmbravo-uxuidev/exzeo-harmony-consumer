d157bf0da1d677ff19e594ff17ae2b8125d7b4b9
Jordan Rhodes <jsrhodes15@gmail.com>
2019-08-14 10:12:09 -0400

    Add jest-dom and configure 'setupTests' file

---
+++ src/setupTests.js
@@ -0,0 +1,23 @@
+import 'jest-dom/extend-expect';
+import { configure as reactTestingConfigure } from '@testing-library/react';
+
+reactTestingConfigure({ testIdAttribute: 'data-test' });
+
+const localStorageMock = {
+  setItem(key, value) {
+    // eslint-disable-next-line no-undef
+    storage[key] = String(value) || '';
+  },
+  getItem(key) {
+    // eslint-disable-next-line no-undef
+    return key in storage ? String(storage[key]) : null;
+  },
+  removeItem(key) {
+    // eslint-disable-next-line no-undef
+    storage[key] = null;
+  }
+};
+
+global.localStorage = localStorageMock;
+
+export default localStorageMock;
