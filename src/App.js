bbe6917cf4bd95ad12875fbb9631afdfd4f38249
Jordan Rhodes <jsrhodes15@gmail.com>
2019-08-15 09:37:33 -0400

    REVERT: move App component into components folder

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import Header from './Header';
-import Footer from './Footer';
-import Navigation from './Navigation';
-import Page from './Page';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+import Page from './components/Page';
 
 const App = () => {
   return (
