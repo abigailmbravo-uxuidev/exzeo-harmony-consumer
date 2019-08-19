0ffb3dae6f9c96d6bc732bb7b6c85bb305acabae
Joe Sutphin <jsutphin@exzeo.com>
2019-08-19 08:23:02 -0400

    add additional search styles

--- src/components/Search/AddressSearch.jsx
+++ src/components/Search/AddressSearch.jsx
@@ -39,7 +39,7 @@ const AddressSearch = () => {
 
               <Button
                 className={Button.constants.classNames.primary}
-                customClass="multi-input btn-search"
+                customClass="multi-input btnSearch"
                 type="submit"
                 data-test="submit"
               >
