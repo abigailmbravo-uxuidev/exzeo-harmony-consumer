022341c25f6c2afaa7af72bcd676183700d8a82e
Abby Bravo <abravo@fl01ws2964.hci01.local>
2019-08-27 09:15:53 -0400

    retrieval address error message styling

--- src/components/Search/NoResults.jsx
+++ src/components/Search/NoResults.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const NoResults = () => {
   return (
     <div>
-      Out of bacon ipsum dolor amet cow andouille tri-tip tongue strip steak
-      chicken kielbasa.
+      We're sorry, we're unable to retrieve the address you've entered. Please
+      note: TypTap only insures single family homes. If the address you have
+      enter does not meet that criteria, it will return in the results!
     </div>
   );
 };
