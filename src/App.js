6bd41c595af0e1a9140d1764ebc676a2ef0265fe
Jordan Rhodes <jsrhodes15@gmail.com>
2019-08-28 11:40:48 -0400

    Add error boundary for routes, and add test route to test/style error boundaries

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Navigation from './components/Navigation';
 import QuoteWorkflow from './components/QuoteWorkflow';
 import Test from './components/Test';
 import { AddressSearch, QuoteSearch } from './components/Search';
+import RouteErrorBoundary from './components/RouteErrorBoundary';
 
 const App = () => {
   return (
@@ -20,30 +21,40 @@ const App = () => {
         <Route children={routeProps => <Navigation {...routeProps} />} />
 
         <main role="main">
-          <Route
-            exact
-            path="/retrieveQuote"
-            render={routeProps => <QuoteSearch />}
-          />
+          <RouteErrorBoundary>
+            <Route
+              exact
+              path="/retrieveQuote"
+              render={routeProps => <QuoteSearch />}
+            />
 
-          <Route
-            exact
-            path="/searchAddress"
-            render={routeProps => <AddressSearch />}
-          />
+            <Route
+              exact
+              path="/searchAddress"
+              render={routeProps => <AddressSearch />}
+            />
 
-          {/* Gandalf owns this */}
-          <Route
-            exact
-            path="/quote"
-            render={routeProps => <QuoteWorkflow {...routeProps} />}
-          />
+            {/* Gandalf owns this */}
+            <Route
+              exact
+              path="/quote"
+              render={routeProps => <QuoteWorkflow {...routeProps} />}
+            />
 
-          <Route
-            exact
-            path="/test"
-            render={routeProps => <Test {...routeProps} />}
-          />
+            <Route
+              exact
+              path="/test"
+              render={routeProps => <Test {...routeProps} />}
+            />
+
+            <Route
+              exact
+              path="/testError"
+              render={routeProps => {
+                throw new Error('Testing error boundaries');
+              }}
+            />
+          </RouteErrorBoundary>
         </main>
       </div>
 
