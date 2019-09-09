cfc01e8d936c51d7d5495e0b0596d8c2bf71b934
Jordan Rhodes <jsrhodes15@gmail.com>
2019-09-09 13:22:38 -0400

    Enable resetting form

--- src/components/Search/QuoteSearch.jsx
+++ src/components/Search/QuoteSearch.jsx
@@ -67,6 +67,11 @@ const QuoteSearch = () => {
     }
   }
 
+  function resetSearch(form) {
+    form.reset();
+    setSearchState(initialState);
+  }
+
   return (
     <div className="view-grid">
       <h1 className="title">Retrieve Quote</h1>
@@ -74,189 +79,193 @@ const QuoteSearch = () => {
         onSubmit={handleSearchSubmit}
         subscription={{ pristine: true, submitting: true }}
       >
-        {({ handleSubmit, submitting, pristine }) => (
-          <form onSubmit={handleSubmit}>
-            <div className="retrieveQuoteWrapper">
-              <Field
-                name="lastName"
-                validate={composeValidators([
-                  validation.isRequired,
-                  validation.isValidNameFormat
-                ])}
-              >
-                {({ input, meta }) => (
-                  <Input
-                    input={input}
-                    meta={meta}
-                    styleName="required"
-                    label="Last Name"
-                    dataTest="lastName"
-                    placeholder="Smith"
-                  />
-                )}
-              </Field>
-
-              <Field
-                name="zipCode"
-                validate={composeValidators([
-                  validation.isRequired,
-                  validation.isZipCode
-                ])}
-              >
-                {({ input, meta }) => (
-                  <Input
-                    input={input}
-                    meta={meta}
-                    type="text"
-                    styleName="required"
-                    label="ZIP Code"
-                    dataTest="zipCode"
-                    placeholder="ZIP Code"
-                  />
-                )}
-              </Field>
-
-              <Field
-                name="quoteNumber"
-                validate={composeValidators([validation.isRequired])}
-              >
-                {({ input, meta }) => (
-                  <Input
-                    input={input}
-                    meta={meta}
-                    styleName="required"
-                    label="Quote Number"
-                    dataTest="quoteNumber"
-                    placeholder="TTIC-A3-"
-                  />
-                )}
-              </Field>
-
-              {/*<Field name="hasQuoteNumber">*/}
-              {/*  {({ input, meta }) => (*/}
-              {/*    <Switch*/}
-              {/*      input={input}*/}
-              {/*      meta={meta}*/}
-              {/*      styleName="switch"*/}
-              {/*      label="Do you have access to the Quote Number"*/}
-              {/*      dataTest="hasQuoteNumber"*/}
-              {/*    />*/}
-              {/*  )}*/}
-              {/*</Field>*/}
-
-              {/*{values.hasQuoteNumber ? (*/}
-              {/*  <Field*/}
-              {/*    name="quoteNumber"*/}
-              {/*    validate={composeValidators([validation.isRequired])}*/}
-              {/*  >*/}
-              {/*    {({ input, meta }) => (*/}
-              {/*      <Input*/}
-              {/*        input={input}*/}
-              {/*        meta={meta}*/}
-              {/*        styleName="required"*/}
-              {/*        label="Quote Number"*/}
-              {/*        dataTest="quoteNumber"*/}
-              {/*        placeholder="TTIC-A3-"*/}
-              {/*      />*/}
-              {/*    )}*/}
-              {/*  </Field>*/}
-              {/*) : (*/}
-              {/*  <Field*/}
-              {/*    name="email"*/}
-              {/*    validate={composeValidators([validation.isRequired])}*/}
-              {/*  >*/}
-              {/*    {({ input, meta }) => (*/}
-              {/*      <Input*/}
-              {/*        input={input}*/}
-              {/*        meta={meta}*/}
-              {/*        styleName="required"*/}
-              {/*        label="Email"*/}
-              {/*        dataTest="email"*/}
-              {/*        placeholder="jsmith@email.com"*/}
-              {/*      />*/}
-              {/*    )}*/}
-              {/*  </Field>*/}
-              {/*)}*/}
-              <div className="form-footer">
-                <Link to="/searchAddress" className="btn btn-secondary">
-                  New Quote
-                </Link>
-                <Button
-                  className={Button.constants.classNames.primary}
-                  type="submit"
-                  data-test="submit"
-                  disabled={submitting || pristine}
+        {({ handleSubmit, submitting, pristine, form }) => (
+          <>
+            <form onSubmit={handleSubmit}>
+              <div className="retrieveQuoteWrapper">
+                <Field
+                  name="lastName"
+                  validate={composeValidators([
+                    validation.isRequired,
+                    validation.isValidNameFormat
+                  ])}
                 >
-                  Retrieve Quote
-                </Button>
-              </div>
-            </div>
-          </form>
-        )}
-      </Form>
+                  {({ input, meta }) => (
+                    <Input
+                      input={input}
+                      meta={meta}
+                      styleName="required"
+                      label="Last Name"
+                      dataTest="lastName"
+                      placeholder="Smith"
+                    />
+                  )}
+                </Field>
 
-      <section className="results">
-        {loading && <SectionLoader />}
+                <Field
+                  name="zipCode"
+                  validate={composeValidators([
+                    validation.isRequired,
+                    validation.isZipCode
+                  ])}
+                >
+                  {({ input, meta }) => (
+                    <Input
+                      input={input}
+                      meta={meta}
+                      type="text"
+                      styleName="required"
+                      label="ZIP Code"
+                      dataTest="zipCode"
+                      placeholder="ZIP Code"
+                    />
+                  )}
+                </Field>
 
-        {searchState.hasSearched && (
-          <React.Fragment>
-            {searchState.noResults && (
-              <Toaster>
-                <div>
-                  Oops! We were unable to find the quote you were looking for.
-                  Please try again or feel free to contact us for support.
-                </div>
-                <button data-test="toaster-cancel" onClick={x => x}>
-                  X
-                </button>
-              </Toaster>
-            )}
+                <Field
+                  name="quoteNumber"
+                  validate={composeValidators([validation.isRequired])}
+                >
+                  {({ input, meta }) => (
+                    <Input
+                      input={input}
+                      meta={meta}
+                      styleName="required"
+                      label="Quote Number"
+                      dataTest="quoteNumber"
+                      placeholder="TTIC-A3-"
+                    />
+                  )}
+                </Field>
 
-            {searchState.result && searchState.invalidQuoteState && (
-              <Modal header="Error Occurred" size={Modal.sizes.medium}>
-                <div>
-                  We apologize but this Quote has a status of{' '}
-                  {searchState.result.quoteState} which is no longer
-                  retrievable. For questions or edits, please contact us.
-                  Provide phone and contact methods. Click Here to start a new
-                  quote.
-                </div>
-                <div className="card-footer">
-                  <Button
-                    className={Button.constants.classNames.secondary}
-                    data-test="reset"
-                    onClick={x => x}
-                  >
-                    Try Again
-                  </Button>
+                {/*<Field name="hasQuoteNumber">*/}
+                {/*  {({ input, meta }) => (*/}
+                {/*    <Switch*/}
+                {/*      input={input}*/}
+                {/*      meta={meta}*/}
+                {/*      styleName="switch"*/}
+                {/*      label="Do you have access to the Quote Number"*/}
+                {/*      dataTest="hasQuoteNumber"*/}
+                {/*    />*/}
+                {/*  )}*/}
+                {/*</Field>*/}
+
+                {/*{values.hasQuoteNumber ? (*/}
+                {/*  <Field*/}
+                {/*    name="quoteNumber"*/}
+                {/*    validate={composeValidators([validation.isRequired])}*/}
+                {/*  >*/}
+                {/*    {({ input, meta }) => (*/}
+                {/*      <Input*/}
+                {/*        input={input}*/}
+                {/*        meta={meta}*/}
+                {/*        styleName="required"*/}
+                {/*        label="Quote Number"*/}
+                {/*        dataTest="quoteNumber"*/}
+                {/*        placeholder="TTIC-A3-"*/}
+                {/*      />*/}
+                {/*    )}*/}
+                {/*  </Field>*/}
+                {/*) : (*/}
+                {/*  <Field*/}
+                {/*    name="email"*/}
+                {/*    validate={composeValidators([validation.isRequired])}*/}
+                {/*  >*/}
+                {/*    {({ input, meta }) => (*/}
+                {/*      <Input*/}
+                {/*        input={input}*/}
+                {/*        meta={meta}*/}
+                {/*        styleName="required"*/}
+                {/*        label="Email"*/}
+                {/*        dataTest="email"*/}
+                {/*        placeholder="jsmith@email.com"*/}
+                {/*      />*/}
+                {/*    )}*/}
+                {/*  </Field>*/}
+                {/*)}*/}
+                <div className="form-footer">
+                  <Link to="/searchAddress" className="btn btn-secondary">
+                    New Quote
+                  </Link>
                   <Button
                     className={Button.constants.classNames.primary}
-                    data-test="new-quote"
-                    onClick={x => x}
+                    type="submit"
+                    data-test="submit"
+                    disabled={submitting || pristine}
                   >
-                    Start New Quote
+                    Retrieve Quote
                   </Button>
                 </div>
-              </Modal>
-            )}
-            {searchState.result && !searchState.invalidQuoteState && (
-              <div>
-                Yay we found your quote: {searchState.result.quoteNumber}
               </div>
-            )}
-          </React.Fragment>
-        )}
+            </form>
+            <section className="results">
+              {loading && <SectionLoader />}
 
-        {/*{searchState.hasSearched &&*/}
-        {/*  (searchState.noResults ? (*/}
-        {/*    <NoResults />*/}
-        {/*  ) : (*/}
-        {/*    <QuoteCard*/}
-        {/*      key={searchState.result.quoteNumber}*/}
-        {/*      quote={searchState.result}*/}
-        {/*    />*/}
-        {/*  ))}*/}
-      </section>
+              {searchState.hasSearched && (
+                <React.Fragment>
+                  {searchState.noResults && (
+                    <Toaster>
+                      <div>
+                        Oops! We were unable to find the quote you were looking
+                        for. Please try again or feel free to contact us for
+                        support.
+                      </div>
+                      <button
+                        data-test="toaster-cancel"
+                        onClick={() => resetSearch(form)}
+                      >
+                        X
+                      </button>
+                    </Toaster>
+                  )}
+
+                  {searchState.result && searchState.invalidQuoteState && (
+                    <Modal header="Error Occurred" size={Modal.sizes.medium}>
+                      <div>
+                        We apologize but this Quote has a status of{' '}
+                        {searchState.result.quoteState} which is no longer
+                        retrievable. For questions or edits, please contact us.
+                        Provide phone and contact methods. Click Here to start a
+                        new quote.
+                      </div>
+                      <div className="card-footer">
+                        <Button
+                          className={Button.constants.classNames.secondary}
+                          data-test="reset"
+                          onClick={() => resetSearch(form)}
+                        >
+                          Try Again
+                        </Button>
+                        <Link
+                          to="/searchAddress"
+                          className={Button.constants.classNames.primary}
+                        >
+                          Start New Quote
+                        </Link>
+                      </div>
+                    </Modal>
+                  )}
+                  {searchState.result && !searchState.invalidQuoteState && (
+                    <div>
+                      Yay we found your quote: {searchState.result.quoteNumber}
+                    </div>
+                  )}
+                </React.Fragment>
+              )}
+
+              {/*{searchState.hasSearched &&*/}
+              {/*  (searchState.noResults ? (*/}
+              {/*    <NoResults />*/}
+              {/*  ) : (*/}
+              {/*    <QuoteCard*/}
+              {/*      key={searchState.result.quoteNumber}*/}
+              {/*      quote={searchState.result}*/}
+              {/*    />*/}
+              {/*  ))}*/}
+            </section>
+          </>
+        )}
+      </Form>
     </div>
   );
 };
