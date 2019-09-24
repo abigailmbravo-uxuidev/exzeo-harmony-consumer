d76f76eaf360548d2c6721f62dff3ac286c5fad1
Jordan Rhodes <jsrhodes15@gmail.com>
2019-09-24 16:42:07 -0400

    Enhance InfoBar to handle no rating object

--- src/components/InfoBar.jsx
+++ src/components/InfoBar.jsx
@@ -3,11 +3,14 @@ import classNames from 'classnames';
 import { format } from '@exzeo/core-ui';
 
 const InfoBar = ({ initialValues, config }) => {
+  const { totalPremium } = initialValues.rating || {};
   return (
     <div className={classNames('infoBar', { className: config.className })}>
       <h2>
         Premium:&nbsp;
-        <strong>{format.toCurrency(initialValues.rating.totalPremium)}</strong>
+        <strong>
+          {totalPremium ? format.toCurrency(totalPremium) : `$ --`}
+        </strong>
       </h2>
       <label>
         Quote Number <strong>{`# ${initialValues.quoteNumber}`}</strong>
