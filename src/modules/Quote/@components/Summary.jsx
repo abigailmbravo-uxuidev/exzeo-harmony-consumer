cc6a9bea0e6a350e9afc8bf5c5e45dd69d51d819
ABravo <abravo@exzeo.com>
2019-09-25 20:24:27 -0400

    styling

--- src/modules/Quote/@components/Summary.jsx
+++ src/modules/Quote/@components/Summary.jsx
@@ -1,16 +1,109 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, ModalPortal } from '@exzeo/core-ui';
-import { ShareModal } from '@exzeo/harmony-core';
+import {
+  Button,
+  noop,
+  Switch,
+  ModalPortal,
+  validation,
+  Field,
+  FormSpy,
+  useField,
+  SectionLoader
+} from '@exzeo/core-ui';
+import {
+  AgencyCard,
+  ShareModal,
+  searchAgencies,
+  AgencyTypeAhead
+} from '@exzeo/harmony-core';
 
+import { useQuote } from 'modules/Quote';
 import TypTapLogo from './TypTapLogo';
 
-const Summary = ({ formInstance }) => {
+const Summary = ({ initialValues, formInstance, formValues }) => {
+  const [editAgency, setEditAgency] = useState(false);
   const [shareQuote, setShareQuote] = useState(false);
+  const [selectedAgency, setSelectedAgency] = useState(null);
+  const { updateQuote, loading } = useQuote();
+  const agentField = useField('agentCode');
+
+  function toggleEditAgency() {
+    setEditAgency(state => {
+      if (state) {
+        formInstance.reset();
+      }
+      return !state;
+    });
+  }
+
+  useEffect(() => {
+    async function getAgency() {
+      const result = await searchAgencies({
+        agencyCode: formValues.agencyCode
+      });
+      const agency = result[0];
+      setSelectedAgency(agency);
+
+      if (initialValues.agencyCode !== formValues.agencyCode) {
+        agentField.input.onChange(Number(agency.agentOfRecord));
+      }
+    }
+
+    getAgency();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formValues.agencyCode]);
 
   return (
     <>
+      {loading && <SectionLoader />}
+
+      {selectedAgency && <AgencyCard agency={selectedAgency} />}
+
+      <Switch
+        label="Do you want to change the agency?"
+        dataTest="test"
+        styleName="switch"
+        input={{
+          name: '',
+          value: editAgency,
+          onChange: () => toggleEditAgency(),
+          onFocus: noop,
+          onBlur: noop
+        }}
+      />
+
+      {editAgency && (
+        <div className="well">
+          <Field name="agencyCode" validate={validation.isRequired}>
+            {({ input, meta }) => (
+              <AgencyTypeAhead
+                dataTest="agency-select"
+                input={input}
+                meta={meta}
+                label="Agencies"
+                styleName="agencyCode"
+              />
+            )}
+          </Field>
+
+          <FormSpy subscription={{ pristine: true, submitting: true }}>
+            {({ pristine, submitting }) => (
+              <Button
+                type="button"
+                className={Button.constants.classNames.primary}
+                data-test="edit-agency"
+                disabled={pristine || submitting}
+                onClick={() => updateQuote(formValues)}
+              >
+                Save
+              </Button>
+            )}
+          </FormSpy>
+        </div>
+      )}
+
       <section className="iconContainer">
         <Link className={Button.constants.classNames.icon} to="underwriting">
           <FontAwesomeIcon icon="edit" />
