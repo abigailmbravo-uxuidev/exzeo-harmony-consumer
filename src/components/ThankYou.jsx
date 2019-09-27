3ddb2e11c7dd738def6d5c44621c02f17174a3ff
ABravo <abravo@exzeo.com>
2019-09-27 12:41:17 -0400

    thank you card

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ThankYou = () => {
-  return <div>Thanks so much for buying Bacon Ipsum from us!</div>;
+  return (
+    <div className="card">
+      <section className="thankYouCircle">
+        <FontAwesomeIcon icon="circle" />
+      </section>
+      <section className="thankYouIcon">
+        <FontAwesomeIcon icon="trophy" />
+      </section>
+      <label className="thankYouTitle">Congratulations!</label>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </p>
+    </div>
+  );
 };
 
 export default ThankYou;
--- src/fontAwesomeInit.js
+++ src/fontAwesomeInit.js
@@ -10,7 +10,8 @@ import {
   faKey,
   faTrophy,
   faExclamationTriangle,
-  faTimes
+  faTimes,
+  faCircle
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
@@ -24,5 +25,6 @@ library.add(
   faKey,
   faTrophy,
   faExclamationTriangle,
-  faTimes
+  faTimes,
+  faCircle
 );
