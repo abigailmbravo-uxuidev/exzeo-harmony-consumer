26613dffd98b81414cfab81b8e1e77a1c9e35008
Jordan Rhodes <jsrhodes15@gmail.com>
2019-10-02 13:29:16 -0400

    Add conditionally rendering field for testing

--- src/modules/Quote/@components/AdditionalInterests.jsx
+++ src/modules/Quote/@components/AdditionalInterests.jsx
@@ -19,47 +19,69 @@ const AdditionalInterests = ({ config, initialValues }) => {
 
   return (
     <div className={config.className}>
-      <Field name="mortgagee1" validate={validation.isRequired}>
-        {({ input, meta }) => (
-          <Radio
-            input={input}
-            meta={meta}
-            answers={BOOL_OPTIONS}
-            styleName="additionalInterest radio"
-            dataTest="mortgagee1"
-            label="Do you have a mortgage?"
-            segmented
-          />
-        )}
-      </Field>
+      <Form onSubmit={x => x}>
+        {({ handleSubmit, values }) => (
+          <>
+            <Field name="mortgagee1" validate={validation.isRequired}>
+              {({ input, meta }) => (
+                <Radio
+                  input={input}
+                  meta={meta}
+                  answers={BOOL_OPTIONS}
+                  styleName="additionalInterest radio"
+                  dataTest="mortgagee1"
+                  label="Do you have a mortgage?"
+                  segmented
+                />
+              )}
+            </Field>
 
-      <Field name="additionalInsured1" validate={validation.isRequired}>
-        {({ input, meta }) => (
-          <Radio
-            input={input}
-            meta={meta}
-            answers={BOOL_OPTIONS}
-            styleName="additionalInterest radio"
-            dataTest="additionalInsured1"
-            label="Are there any additional insureds on the property (other than the mortgage company)?"
-            segmented
-          />
-        )}
-      </Field>
+            {values.mortgagee1 === true && (
+              <Field name="mortgagee2" validate={validation.isRequired}>
+                {({ input, meta }) => (
+                  <Radio
+                    input={input}
+                    meta={meta}
+                    answers={BOOL_OPTIONS}
+                    styleName="additionalInterest radio"
+                    dataTest="mortgagee2"
+                    label="Do you have another mortgage (someone else that can take your house if you don't pay)?"
+                    segmented
+                  />
+                )}
+              </Field>
+            )}
 
-      <Field name="additionalInterest1" validate={validation.isRequired}>
-        {({ input, meta }) => (
-          <Radio
-            input={input}
-            meta={meta}
-            answers={BOOL_OPTIONS}
-            styleName="additionalInterest radio"
-            dataTest="additionalInterest1"
-            label="Are there any additional interests?"
-            segmented
-          />
+            <Field name="additionalInsured1" validate={validation.isRequired}>
+              {({ input, meta }) => (
+                <Radio
+                  input={input}
+                  meta={meta}
+                  answers={BOOL_OPTIONS}
+                  styleName="additionalInterest radio"
+                  dataTest="additionalInsured1"
+                  label="Are there any additional insureds on the property (other than the mortgage company)?"
+                  segmented
+                />
+              )}
+            </Field>
+
+            <Field name="additionalInterest1" validate={validation.isRequired}>
+              {({ input, meta }) => (
+                <Radio
+                  input={input}
+                  meta={meta}
+                  answers={BOOL_OPTIONS}
+                  styleName="additionalInterest radio"
+                  dataTest="additionalInterest1"
+                  label="Are there any additional interests?"
+                  segmented
+                />
+              )}
+            </Field>
+          </>
         )}
-      </Field>
+      </Form>
     </div>
   );
 };
