e12a35ac23e4bca33ba19ce7976abf65119234cc
Jordan Rhodes <jsrhodes15@gmail.com>
2019-10-14 16:11:04 -0400

    Display FEMAfloodZone instead of floodZone

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -46,7 +46,7 @@ const Navigation = ({ location, match }) => {
                 <strong>{quote.property.physicalAddress.address1}</strong>
               </p>
               <p>{format.toCityStateZip(quote.property.physicalAddress)}</p>
-              <p>{`FEMA Flood Zone: "${quote.property.floodZone}"`}</p>
+              <p>{`FEMA Flood Zone: "${quote.property.FEMAfloodZone}"`}</p>
               <p>{`Year built: ${quote.property.yearBuilt}`}</p>
             </React.Fragment>
           ) : (
