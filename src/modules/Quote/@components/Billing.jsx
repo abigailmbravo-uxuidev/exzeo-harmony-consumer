875f9217355d522b0e23e5ee1a2f685ea390f49f
Jordan Rhodes <jsrhodes15@gmail.com>
2019-10-14 16:11:22 -0400

    Show billing options as Cards

--- src/modules/Quote/@components/Billing.jsx
+++ src/modules/Quote/@components/Billing.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { SectionLoader } from '@exzeo/core-ui';
 import { useFetchBillingConfiguration } from '@exzeo/harmony-core';
 
-const Billing = () => {
-  return <div>Bill things</div>;
+const Billing = ({ initialValues }) => {
+  const { billingConfig, loaded } = useFetchBillingConfiguration(initialValues);
+
+  if (!loaded) {
+    return <SectionLoader />;
+  }
+
+  return (
+    <div>
+      {billingConfig.billingOptions.map(option => (
+        <div key={option.answer} className="card" data-test="billToCard">
+          <div className="cardContent">
+            <h4>{option.label.split(' ').filter((s, i) => i !== 0)}</h4>
+          </div>
+        </div>
+      ))}
+
+      {/*<pre>{JSON.stringify(billingConfig, 0, 2)}</pre>*/}
+    </div>
+  );
 };
 
 export default Billing;
