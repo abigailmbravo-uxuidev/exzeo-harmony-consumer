a282120804d45c16dc56b8c992666bb654490daf
Mark Eads <meads@hcpci.com>
2019-10-16 16:13:14 -0400

    update agency details

--- src/modules/Quote/@components/AgencyDetails.jsx
+++ src/modules/Quote/@components/AgencyDetails.jsx
@@ -1,9 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SummaryGroup from './SummaryGroup';
-import { noop } from '@exzeo/core-ui/src';
+import { noop, SectionLoader } from '@exzeo/core-ui/src';
+import { searchAgencies } from '@exzeo/harmony-core';
 
-const AgencyDetails = ({ renderChildren }) => {
+const AgencyDetails = ({ initialValues }) => {
   const [confirmAgency, setConfirmAgency] = useState(false);
+  const [selectedAgency, setSelectedAgency] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    async function getAgency() {
+      setLoaded(false);
+      const result = await searchAgencies({
+        agencyCode: initialValues.agencyCode
+      });
+
+      setSelectedAgency(result[0]);
+      setLoaded(true);
+    }
+
+    getAgency();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [initialValues.agencyCode]);
+
+  if (!loaded) {
+    return <SectionLoader />;
+  }
 
   return (
     <React.Fragment>
@@ -15,7 +37,12 @@ const AgencyDetails = ({ renderChildren }) => {
         switchOnChange={value => setConfirmAgency(value)}
         handleEditClick={noop}
       >
-        {renderChildren && renderChildren()}
+        <dl className="agent">
+          <div>
+            <dt>Agency</dt>
+            <dd>{selectedAgency.displayName}</dd>
+          </div>
+        </dl>
       </SummaryGroup>
     </React.Fragment>
   );
