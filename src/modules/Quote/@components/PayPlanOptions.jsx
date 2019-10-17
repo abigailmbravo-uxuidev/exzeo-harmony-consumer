bd88c0da34d4b997f245d4d810b83ed67d8f5805
ABravo <abravo@exzeo.com>
2019-10-17 14:22:16 -0500

    html and css for card styling on billing

--- src/modules/Quote/@components/PayPlanOptions.jsx
+++ src/modules/Quote/@components/PayPlanOptions.jsx
@@ -19,16 +19,26 @@ const PayPlanOptions = ({
           })}
           onClick={() => handleClick(PAY_PLANS.annual)}
         >
+          <h5>Annual</h5>
           <dl className="" data-test="annual-plan">
-            <dt>Annual</dt>
-            <dd>
-              {format.toCurrency(paymentPlan.amount)} :{' '}
-              {date.formatDate(paymentPlan.dueDate)}
-            </dd>
-            <label>
-              {selectedPlan === PAY_PLANS.annual ? 'Selected' : 'Select'}
-            </label>
+            <div>
+              <dd>
+                <span>
+                  <strong>Due on:&nbsp;</strong>
+                </span>
+                {date.formatDate(paymentPlan.dueDate)}
+              </dd>
+              <dd>
+                <span>
+                  <strong>Amount:&nbsp;</strong>
+                </span>
+                {format.toCurrency(paymentPlan.amount)}
+              </dd>
+            </div>
           </dl>
+          <label>
+            {selectedPlan === PAY_PLANS.annual ? 'Selected' : 'Select'}
+          </label>
         </div>
       )}
       {paymentPlan && paymentPlan.s1 && paymentPlan.s2 && (
@@ -38,20 +48,41 @@ const PayPlanOptions = ({
           })}
           onClick={() => handleClick(PAY_PLANS.semiAnnual)}
         >
+          <h5>Semi-Annual</h5>
           <dl className="" data-test="semi-annual-plan">
-            <dt>Semi-Annual</dt>
-            <dd>
-              {format.toCurrency(paymentPlan.s1.amount)} :{' '}
-              {date.formatDate(paymentPlan.s1.dueDate)}
-            </dd>
-            <dd>
-              {format.toCurrency(paymentPlan.s2.amount)} :{' '}
-              {date.formatDate(paymentPlan.s2.dueDate)}
-            </dd>
-            <label>
-              {selectedPlan === PAY_PLANS.semiAnnual ? 'Selected' : 'Select'}
-            </label>
+            <div className="semiCard">
+              <dd>
+                <span>
+                  <strong>Due on:&nbsp;</strong>
+                </span>
+                {date.formatDate(paymentPlan.s1.dueDate)}
+              </dd>
+              <dd>
+                <span>
+                  <strong>Amount:&nbsp;</strong>
+                </span>
+                {format.toCurrency(paymentPlan.s1.amount)}
+              </dd>
+            </div>
+
+            <div>
+              <dd>
+                <span>
+                  <strong>Due on:&nbsp;</strong>
+                </span>
+                {date.formatDate(paymentPlan.s2.dueDate)}
+              </dd>
+              <dd>
+                <span>
+                  <strong>Amount:&nbsp;</strong>
+                </span>
+                {format.toCurrency(paymentPlan.s2.amount)}
+              </dd>
+            </div>
           </dl>
+          <label>
+            {selectedPlan === PAY_PLANS.semiAnnual ? 'Selected' : 'Select'}
+          </label>
         </div>
       )}
       {paymentPlan &&
@@ -65,28 +96,71 @@ const PayPlanOptions = ({
             })}
             onClick={() => handleClick(PAY_PLANS.quarterly)}
           >
+            <h5>Quarterly</h5>
             <dl className="" data-test="quarterly-plan">
-              <dt>Quarterly</dt>
-              <dd>
-                {format.toCurrency(paymentPlan.q1.amount)} :{' '}
-                {date.formatDate(paymentPlan.q1.dueDate)}
-              </dd>
-              <dd>
-                {format.toCurrency(paymentPlan.q2.amount)} :{' '}
-                {date.formatDate(paymentPlan.q2.dueDate)}
-              </dd>
-              <dd>
-                {format.toCurrency(paymentPlan.q3.amount)} :{' '}
-                {date.formatDate(paymentPlan.q3.dueDate)}
-              </dd>
-              <dd>
-                {format.toCurrency(paymentPlan.q4.amount)} :{' '}
-                {date.formatDate(paymentPlan.q4.dueDate)}
-              </dd>
-              <label>
-                {selectedPlan === PAY_PLANS.quarterly ? 'Selected' : 'Select'}
-              </label>
+              <div>
+                <dd>
+                  <span>
+                    <strong>Due on:&nbsp;</strong>
+                  </span>
+                  {date.formatDate(paymentPlan.q1.dueDate)}
+                </dd>
+                <dd>
+                  <span>
+                    <strong>Amount:&nbsp;</strong>
+                  </span>
+                  {format.toCurrency(paymentPlan.q1.amount)}
+                </dd>
+              </div>
+
+              <div>
+                <dd>
+                  <span>
+                    <strong>Due on:&nbsp;</strong>
+                  </span>
+                  {date.formatDate(paymentPlan.q2.dueDate)}
+                </dd>
+                <dd>
+                  <span>
+                    <strong>Amount:&nbsp;</strong>
+                  </span>
+                  {format.toCurrency(paymentPlan.q2.amount)}
+                </dd>
+              </div>
+
+              <div>
+                <dd>
+                  <span>
+                    <strong>Due on:&nbsp;</strong>
+                  </span>
+                  {date.formatDate(paymentPlan.q3.dueDate)}
+                </dd>
+                <dd>
+                  <span>
+                    <strong>Amount:&nbsp;</strong>
+                  </span>
+                  {format.toCurrency(paymentPlan.q3.amount)}
+                </dd>
+              </div>
+
+              <div>
+                <dd>
+                  <span>
+                    <strong>Due on:&nbsp;</strong>
+                  </span>
+                  {date.formatDate(paymentPlan.q4.dueDate)}
+                </dd>
+                <dd>
+                  <span>
+                    <strong>Amount:&nbsp;</strong>
+                  </span>
+                  {format.toCurrency(paymentPlan.q4.amount)}
+                </dd>
+              </div>
             </dl>
+            <label>
+              {selectedPlan === PAY_PLANS.quarterly ? 'Selected' : 'Select'}
+            </label>
           </div>
         )}
     </div>
