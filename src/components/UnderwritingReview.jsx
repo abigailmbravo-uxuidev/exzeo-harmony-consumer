fe42c8f4063f1c17e3a88b8d782a59b3c0fab420
Joe Sutphin <jsutphin@exzeo.com>
2019-10-18 13:16:16 -0400

    update UW error page structure

--- src/components/UnderwritingReview.jsx
+++ src/components/UnderwritingReview.jsx
@@ -6,24 +6,27 @@ import ContactFooter from './ContactFooter';
 
 const UnderwritingReview = () => {
   return (
-    <div className="modal">
-      <div className="card UWReview">
-        <h1>Thank you for quoting with Typtap!</h1>
-        <p>
-          You have completed the requirements to obtain your Flood Quote. To
-          continue to purchase your policy, please contact one of our
-          representatives.
-        </p>
-        <ContactFooter>
-          <Link
-            to="customize"
-            className={Button.constants.classNames.primary}
-            data-test="edit-quote"
-          >
-            Edit Quote
-          </Link>
-        </ContactFooter>
-      </div>
+    <div role="region">
+      <nav role="navigation" className="navWrapper navSlideIn"></nav>
+      <main role="main">
+        <div className="UWReview">
+          <h1>Thank you for quoting with Typtap!</h1>
+          <p>
+            You have completed the requirements to obtain your Flood Quote. To
+            continue to purchase your policy, please contact one of our
+            representatives.
+          </p>
+          <ContactFooter>
+            <Link
+              to="customize"
+              className={Button.constants.classNames.primary}
+              data-test="edit-quote"
+            >
+              Edit Quote
+            </Link>
+          </ContactFooter>
+        </div>
+      </main>
     </div>
   );
 };
