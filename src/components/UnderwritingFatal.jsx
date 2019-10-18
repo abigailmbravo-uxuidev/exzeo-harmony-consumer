9940ea6a1fc1c3f12bb1ea8be3206aa369188a52
Joe Sutphin <jsutphin@exzeo.com>
2019-10-18 12:47:36 -0400

    add modal dialog

--- src/components/UnderwritingFatal.jsx
+++ src/components/UnderwritingFatal.jsx
@@ -5,16 +5,18 @@ import TypTapLink from './TypTapLink';
 
 const UnderwritingFatal = () => {
   return (
-    <div className="card">
-      <h1>We're So Sorry!</h1>
-      <p>
-        Property does not qualify. We apologize, we are unable to provide an
-        automated Flood Quote for your property at this time. Please contact one
-        of our representatives so they may further assist you.
-      </p>
-      <ContactFooter>
-        <TypTapLink />
-      </ContactFooter>
+    <div className="modal">
+      <div className="card UWFatal">
+        <h1>We're So Sorry!</h1>
+        <p>
+          Property does not qualify. We apologize, we are unable to provide an
+          automated Flood Quote for your property at this time. Please contact
+          one of our representatives so they may further assist you.
+        </p>
+        <ContactFooter>
+          <TypTapLink />
+        </ContactFooter>
+      </div>
     </div>
   );
 };
--- src/components/UnderwritingReview.jsx
+++ src/components/UnderwritingReview.jsx
@@ -6,22 +6,24 @@ import ContactFooter from './ContactFooter';
 
 const UnderwritingReview = () => {
   return (
-    <div className="card">
-      <h1>Thank you for quoting with Typtap!</h1>
-      <p>
-        You have completed the requirements to obtain your Flood Quote. To
-        continue to purchase your policy, please contact one of our
-        representatives.
-      </p>
-      <ContactFooter>
-        <Link
-          to="customize"
-          className={Button.constants.classNames.primary}
-          data-test="edit-quote"
-        >
-          Edit Quote
-        </Link>
-      </ContactFooter>
+    <div className="modal">
+      <div className="card UWReview">
+        <h1>Thank you for quoting with Typtap!</h1>
+        <p>
+          You have completed the requirements to obtain your Flood Quote. To
+          continue to purchase your policy, please contact one of our
+          representatives.
+        </p>
+        <ContactFooter>
+          <Link
+            to="customize"
+            className={Button.constants.classNames.primary}
+            data-test="edit-quote"
+          >
+            Edit Quote
+          </Link>
+        </ContactFooter>
+      </div>
     </div>
   );
 };
