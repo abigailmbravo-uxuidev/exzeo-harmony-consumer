0583e4f827f0f15deae545f1c730d6873de745f3
Joe Sutphin <jsutphin@exzeo.com>
2019-10-22 08:53:59 -0400

    update structure of UW review modal

--- src/components/UnderwritingReview.jsx
+++ src/components/UnderwritingReview.jsx
@@ -9,34 +9,36 @@ const UnderwritingReview = () => {
     <div role="region">
       <nav role="navigation" className="navWrapper navSlideIn"></nav>
       <main role="main">
-        <div className="UWReview">
-          <h1>Quote Under Review</h1>
-          <div className="infoBar">
-            <h2>
-              Annual Premium:&nbsp;<strong>$275</strong>
-            </h2>
-            <label>
-              Quote Number:&nbsp;<strong>#77742</strong>
-            </label>
-          </div>
-          <div className="subtitle">
-            <strong>Thank you for quoting with Typtap!</strong>
-            <p>
-              You have completed the requirements to obtain your Flood Quote. To
-              continue to purchase your policy, please contact one of our
-              representatives.
-            </p>
-          </div>
-          <div className="needsUpdating">
-            <ContactFooter>
-              <Link
-                to="customize"
-                className={Button.constants.classNames.primary}
-                data-test="edit-quote"
-              >
-                Edit Quote
-              </Link>
-            </ContactFooter>
+        <div className="view-grid">
+          <div className="UWReview">
+            <div className="title">Quote Under Review</div>
+            <div className="infoBar">
+              <h2>
+                Annual Premium:&nbsp;<strong>$275</strong>
+              </h2>
+              <label>
+                Quote Number:&nbsp;<strong>#77742</strong>
+              </label>
+            </div>
+            <div className="subtitle">
+              <strong>Thank you for quoting with Typtap!</strong>
+              <p>
+                You have completed the requirements to obtain your Flood Quote.
+                To continue to purchase your policy, please contact one of our
+                representatives.
+              </p>
+            </div>
+            <div className="needsUpdating">
+              <ContactFooter>
+                <Link
+                  to="customize"
+                  className={Button.constants.classNames.primary}
+                  data-test="edit-quote"
+                >
+                  Edit Quote
+                </Link>
+              </ContactFooter>
+            </div>
           </div>
         </div>
       </main>
