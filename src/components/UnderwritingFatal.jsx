4da9643143b64e047d2b06357c561899b43f8341
Joe Sutphin <jsutphin@exzeo.com>
2019-10-22 09:15:34 -0400

    update fatal error structure

--- src/components/UnderwritingFatal.jsx
+++ src/components/UnderwritingFatal.jsx
@@ -8,16 +8,18 @@ const UnderwritingFatal = () => {
     <div role="region">
       <nav role="navigation" className="navWrapper navSlideIn"></nav>
       <main role="main">
-        <div className="UWFatal">
-          <h1>We're So Sorry!</h1>
-          <p>
-            Property does not qualify. We apologize, we are unable to provide an
-            automated Flood Quote for your property at this time. Please contact
-            one of our representatives so they may further assist you.
-          </p>
-          <ContactFooter>
-            <TypTapLink />
-          </ContactFooter>
+        <div className="view-grid">
+          <div className="UWFatal">
+            <div className="title">We're So Sorry!</div>
+            <p>
+              Property does not qualify. We apologize, we are unable to provide
+              an automated Flood Quote for your property at this time. Please
+              contact one of our representatives so they may further assist you.
+            </p>
+            <ContactFooter>
+              <TypTapLink />
+            </ContactFooter>
+          </div>
         </div>
       </main>
     </div>
