5aec3c9d32d28d6daeacca46eb13fdc59458ea5a
Jordan Rhodes <jsrhodes15@gmail.com>
2019-10-28 16:04:45 -0400

    Show all three types of additional interests on Summary page

--- src/modules/Quote/@components/AdditionalInterestsDetails.jsx
+++ src/modules/Quote/@components/AdditionalInterestsDetails.jsx
@@ -1,44 +1,75 @@
 import React from 'react';
-import SummaryGroup from './SummaryGroup';
-import { noop } from '@exzeo/core-ui/src';
 import { Link } from 'react-router-dom';
+import { noop } from '@exzeo/core-ui/src';
+import {
+  V2AdditionalInterestCard,
+  useAdditionalInterests,
+  AI_TYPES
+} from '@exzeo/core-ui/src/@Harmony';
+import SummaryGroup from './SummaryGroup';
 
-const AdditionalInterestsDetails = ({ initialValues, renderChildren }) => {
+const AdditionalInterestsDetails = ({ initialValues }) => {
+  const { groupedAdditionalInterests } = useAdditionalInterests(initialValues);
+
   return (
-    <React.Fragment>
-      <SummaryGroup
-        header="Additional Insured Details"
-        detailClass="ai-details"
-        name="confirmAdditionalInterest"
-        link="additionalInfo"
-        handleEditClick={noop}
-      >
-        {renderChildren && renderChildren()}
-        {initialValues.additionalInterests.filter(a => a.type === 'Mortgagee')
-          .length === 0 && (
-          <dl className="ai">
-            <div>
-              <dt>No mortgages added.</dt>
-              <dd>
-                <Link to="additionalInfo">Add mortgagee now?</Link>
-              </dd>
-            </div>
-          </dl>
-        )}
-        {initialValues.additionalInterests.filter(
-          a => a.type === 'Additional Insured'
-        ).length === 0 && (
-          <dl className="ai">
-            <div>
-              <dt>No additional insured added.</dt>
-              <dd>
-                <Link to="additionalInfo">Add additional insured now?</Link>
-              </dd>
-            </div>
-          </dl>
-        )}
-      </SummaryGroup>
-    </React.Fragment>
+    <SummaryGroup
+      header="Additional Insured Details"
+      detailClass="ai-details"
+      name="confirmAdditionalInterest"
+      link="additionalInfo"
+      handleEditClick={noop}
+    >
+      {groupedAdditionalInterests[AI_TYPES.mortgagee].length ? (
+        <ul className="listItem">
+          {groupedAdditionalInterests[AI_TYPES.mortgagee].map(ai => (
+            <V2AdditionalInterestCard key={ai._id} ai={ai} />
+          ))}
+        </ul>
+      ) : (
+        <dl className="ai">
+          <div>
+            <dt>No mortgages added.</dt>
+            <dd>
+              <Link to="additionalInfo">Add mortgagee now?</Link>
+            </dd>
+          </div>
+        </dl>
+      )}
+
+      {groupedAdditionalInterests[AI_TYPES.additionalInsured].length ? (
+        <ul className="listItem">
+          {groupedAdditionalInterests[AI_TYPES.additionalInsured].map(ai => (
+            <V2AdditionalInterestCard key={ai._id} ai={ai} />
+          ))}
+        </ul>
+      ) : (
+        <dl className="ai">
+          <div>
+            <dt>No additional insured added.</dt>
+            <dd>
+              <Link to="additionalInfo">Add additional insured now?</Link>
+            </dd>
+          </div>
+        </dl>
+      )}
+
+      {groupedAdditionalInterests[AI_TYPES.additionalInterest].length ? (
+        <ul className="listItem">
+          {groupedAdditionalInterests[AI_TYPES.additionalInterest].map(ai => (
+            <V2AdditionalInterestCard key={ai._id} ai={ai} />
+          ))}
+        </ul>
+      ) : (
+        <dl className="ai">
+          <div>
+            <dt>No additional interest added.</dt>
+            <dd>
+              <Link to="additionalInfo">Add additional interest now?</Link>
+            </dd>
+          </div>
+        </dl>
+      )}
+    </SummaryGroup>
   );
 };
 
