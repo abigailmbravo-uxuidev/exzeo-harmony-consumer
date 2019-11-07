1cabb52a9d0c7a04eeb78e25609070fe9d9ae3ef
Jordan Rhodes <jsrhodes15@gmail.com>
2019-11-07 18:37:08 -0500

    Change PolicyHolder section
    Add policyHolders one by one by clicking on the 'plus' button

--- src/modules/Quote/@components/PolicyHolder.jsx
+++ src/modules/Quote/@components/PolicyHolder.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {
+  Button,
   Field,
   Form,
   ModalPortal,
@@ -15,14 +16,9 @@ import PolicyholderCard from './PolicyholderCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const INITIAL_STATE = {
-  show: false
+  show: false,
+  ph: null
 };
-
-function setInitialValues(quote) {
-  return {
-    policyHolder2: quote.policyHolders.length > 1
-  };
-}
 
 const PolicyHolder = ({ initialValues, config, customHandlers }) => {
   const [modal, setModal] = useState(INITIAL_STATE);
@@ -47,102 +43,43 @@ const PolicyHolder = ({ initialValues, config, customHandlers }) => {
     setModal(INITIAL_STATE);
   }
 
-  function deleteSecondaryPolicyHolder() {
-    const data = {
-      ...initialValues,
-      policyHolders: [initialValues.policyHolders[0]]
-    };
-
-    customHandlers.handleSubmit(data);
-  }
-
   return (
     <section className={config.className}>
-      <Form onSubmit={x => x} initialValues={setInitialValues(initialValues)}>
-        {({ values, form }) => (
-          <React.Fragment>
-            <Field name="policyHolder2" validate={validation.isRequired}>
-              {({ input, meta }) => (
-                <Radio
-                  input={input}
-                  meta={meta}
-                  answers={BOOL_OPTIONS}
-                  styleName="policyHolder radio"
-                  dataTest="policyHolder2"
-                  label="Is there an additional owner's name to add (e.g. spouse on deed and/or living in home)?"
-                  hint=""
-                  segmented
-                />
-              )}
-            </Field>
+      <h4>Policyholder(s)</h4>
+      {initialValues.policyHolders.map(ph => (
+        <PolicyholderCard
+          key={ph.order}
+          policyHolder={ph}
+          icons={
+            <a onClick={() => setModal({ show: true, ph: ph.order })}>
+              <FontAwesomeIcon icon="edit" />
+            </a>
+          }
+        />
+      ))}
 
-            {modal.show && modal.ph === 0 && (
-              <ModalPortal>
-                <PolicyHolderModal
-                  handleFormSubmit={handlePolicyHolderSubmit}
-                  handleCancel={() => setModal(INITIAL_STATE)}
-                  initialValues={initialValues.policyHolders[0]}
-                  order={0}
-                />
-              </ModalPortal>
-            )}
+      <Button
+        data-test="add-policyHolder"
+        onClick={() =>
+          setModal({ show: true, ph: initialValues.policyHolders.length })
+        }
+        disabled={initialValues.policyHolders.length > 1}
+        className={Button.constants.classNames.icon}
+        type="button"
+      >
+        +
+      </Button>
 
-            {((values.policyHolder2 && !initialValues.policyHolders[1]) ||
-              (modal.show && modal.ph === 1)) && (
-              <ModalPortal>
-                <PolicyHolderModal
-                  initialValues={initialValues.policyHolders[1]}
-                  fieldPrefix="ph"
-                  order={1}
-                  handleFormSubmit={handlePolicyHolderSubmit}
-                  handleCancel={() =>
-                    modal.show
-                      ? setModal(INITIAL_STATE)
-                      : form.change('policyHolder2', false)
-                  }
-                />
-              </ModalPortal>
-            )}
-
-            <h4>Policyholder 1</h4>
-            <PolicyholderCard
-              policyHolder={initialValues.policyHolders[0]}
-              icons={
-                <a onClick={() => setModal({ show: true, ph: 0 })}>
-                  <FontAwesomeIcon icon="edit" />
-                </a>
-              }
-            />
-
-            {initialValues.policyHolders[1] && (
-              <React.Fragment>
-                <h4>Policyholder 2</h4>
-                <PolicyholderCard
-                  policyHolder={initialValues.policyHolders[1]}
-                  icons={
-                    <React.Fragment>
-                      <a onClick={() => setModal({ show: true, ph: 1 })}>
-                        <FontAwesomeIcon icon="edit" />
-                      </a>
-                      <a onClick={deleteSecondaryPolicyHolder}>
-                        <FontAwesomeIcon icon="times" />
-                      </a>
-                    </React.Fragment>
-                  }
-                />
-              </React.Fragment>
-            )}
-
-            <OnChangeListener name="policyHolder2">
-              {value => {
-                if (!value && initialValues.policyHolders[1]) {
-                  deleteSecondaryPolicyHolder();
-                }
-              }}
-            </OnChangeListener>
-          </React.Fragment>
-        )}
-      </Form>
+      {modal.show && (
+        <ModalPortal>
+          <PolicyHolderModal
+            handleFormSubmit={handlePolicyHolderSubmit}
+            handleCancel={() => setModal(INITIAL_STATE)}
+            initialValues={initialValues.policyHolders[modal.ph]}
+            order={modal.ph}
+          />
+        </ModalPortal>
+      )}
     </section>
   );
 };
