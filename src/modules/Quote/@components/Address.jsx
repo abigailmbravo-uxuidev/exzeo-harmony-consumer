0c9ef18c7d0ddc36abe10230a8d840fa6e6046ea
Jordan Rhodes <jsrhodes15@gmail.com>
2019-11-07 18:53:38 -0500

    WIP - Update policyHolderMailingAddress behavior

--- src/modules/Quote/@components/Address.jsx
+++ src/modules/Quote/@components/Address.jsx
@@ -1,47 +1,62 @@
-import React from 'react';
-import { Field, FormSpy, Radio } from '@exzeo/core-ui';
+import React, { useState } from 'react';
+import { Field, FormSpy, Radio, Modal, Button } from '@exzeo/core-ui';
 import classNames from 'classnames';
-import {
-  AddressFormFields,
-  AddressFieldWatchers
-} from '@exzeo/core-ui/src/@Harmony';
+import { AddressWithAutoFill } from '@exzeo/core-ui/src/@Harmony';
+
 import { BOOL_OPTIONS } from '../../../constants/input';
+import AddressCard from './AddressCard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Address = ({ initialValues }) => {
+  const [modal, setModal] = useState({ show: false });
+
   return (
     <section className="addressSection">
-      <Field name="sameAsPropertyAddress">
-        {({ input, meta }) => (
-          <Radio
-            input={input}
-            meta={meta}
-            answers={BOOL_OPTIONS}
-            label="Is your mailing address the same as the property address?"
-            styleName="mailingSameAsProperty radio"
-            dataTest="mailingSameAsProperty"
-            segmented
+      {initialValues.policyHolderMailingAddress.address1 && (
+        <AddressCard
+          address={initialValues.policyHolderMailingAddress}
+          icons={
+            <a onClick={() => setModal({ show: true })}>
+              <FontAwesomeIcon icon="edit" />
+            </a>
+          }
+        />
+      )}
+
+      <Button
+        data-test="add-address"
+        onClick={() => setModal({ show: true })}
+        disabled={initialValues.policyHolderMailingAddress.address1}
+        className={Button.constants.classNames.icon}
+        type="button"
+      >
+        +
+      </Button>
+
+      {modal.show && (
+        <Modal size={Modal.sizes.xlarge} className="" header="">
+          <Field name="sameAsPropertyAddress">
+            {({ input, meta }) => (
+              <Radio
+                input={input}
+                meta={meta}
+                answers={BOOL_OPTIONS}
+                label="Is your mailing address the same as the property address?"
+                styleName="mailingSameAsProperty radio"
+                dataTest="mailingSameAsProperty"
+                segmented
+              />
+            )}
+          </Field>
+
+          <AddressWithAutoFill
+            watchField="sameAsPropertyAddress"
+            fieldPrefix="policyHolderMailingAddress"
+            matchPrefix="property.physicalAddress"
+            values={initialValues}
           />
-        )}
-      </Field>
-
-      <FormSpy subscription={{ values: true }}>
-        {({ values }) => (
-          <div
-            className={classNames('well', {
-              hide: values.sameAsPropertyAddress
-            })}
-          >
-            <AddressFormFields fieldPrefix="policyHolderMailingAddress" />
-          </div>
-        )}
-      </FormSpy>
-
-      <AddressFieldWatchers
-        watchField="sameAsPropertyAddress"
-        fieldPrefix="policyHolderMailingAddress"
-        matchPrefix="property.physicalAddress"
-        values={initialValues}
-      />
+        </Modal>
+      )}
     </section>
   );
 };
