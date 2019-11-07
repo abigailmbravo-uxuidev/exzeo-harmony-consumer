158e1a7539107d64866f7784f69184e654ea3f9a
ABravo <abravo@exzeo.com>
2019-11-07 08:20:50 -0500

    congrats page text

--- src/modules/Quote/@components/WorkflowComplete.jsx
+++ src/modules/Quote/@components/WorkflowComplete.jsx
@@ -206,7 +206,40 @@ const ThankYou = () => {
         </svg>
       </section>
       <label className="congratsTitle">Congratulations!</label>
-      <p>You have completed all the steps.</p>
+      <p>You have completed all the steps for your TypTap Flood Policy.</p>
+      <p>
+        1. You will be receiving an e-mail (to the e-mail address you provided),
+        containing your insurance application. Please review the information and
+        sign the application.
+      </p>
+
+      <p>
+        2. Once you have signed the application, it will be routed to any other
+        additional policyholders for their signature.
+      </p>
+
+      <p>
+        3. Upon completion of all required signatures, we will issue your
+        policy.
+      </p>
+
+      <p>
+        4. The above steps need to be completed within 10 days to create your
+        policy.
+      </p>
+
+      <p>
+        5. A copy of the policy, and required documents will then be sent to
+        your email.
+      </p>
+      <p>
+        Thank you for your business! If you have any questions or concerns,
+        please contact us.
+      </p>
+
+      <a href="https://typtap.com/" target="_self" rel="noopener noreferrer">
+        www.typtap.com
+      </a>
     </div>
   );
 };
