8fd171c263bc4d4dd3ae595cf0fac81889469a8d
Jordan Rhodes <jsrhodes15@gmail.com>
2019-11-08 15:50:19 -0500

    Make entire div around Card and Checkbox clickable

--- src/modules/Quote/@components/BillingOption.jsx
+++ src/modules/Quote/@components/BillingOption.jsx
@@ -4,15 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const BillingOption = ({ option, handleClick, isSelected }) => {
   return (
     <React.Fragment>
-      <div className="billingCheckBox">
+      <div
+        className="billingCheckBox"
+        onClick={() => handleClick(option.answer)}
+      >
         <span className="emptyCheckBox">
           <FontAwesomeIcon icon={isSelected ? 'check' : ''} />
         </span>
-        <div
-          className="card billToId"
-          data-test="billToCard"
-          onClick={() => handleClick(option.answer)}
-        >
+        <div className="card billToId" data-test="billToCard">
           <div className="cardContent">
             <h4>{option.label}</h4>
           </div>
