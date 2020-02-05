c3b5532689f7361bc111825129903dc835138588
Jordan Rhodes <jsrhodes15@gmail.com>
2020-02-05 18:05:29 -0500

    Add SC-AF3 and NJ-AF3 quote and policy templates, refactor how we name them

--- src/modules/Quote/hooks.js
+++ src/modules/Quote/hooks.js
@@ -1,15 +1,24 @@
 import { useState, useEffect } from 'react';
 
-import MOCK_AF3_TEMPLATE from 'mock-data/mockAF3';
+import TTICFLAF3 from 'csp-templates/ttic-fl-af3-quote';
+import HCPCNJAF3 from 'csp-templates/hcpc-nj-af3-quote';
+import HCPCSCAF3 from 'csp-templates/hcpc-sc-af3-quote';
 
-export function useWorkflowTemplate({ product }) {
+const TEMPLATES = {
+  'TTIC:FL:AF3': TTICFLAF3,
+  'HCPC:NJ:AF3': HCPCNJAF3,
+  'HCPC:SC:AF3': HCPCSCAF3
+};
+
+export function useWorkflowTemplate({ companyCode, state, product }) {
   const [template, setTemplate] = useState(null);
 
   useEffect(() => {
-    if (product) {
-      setTemplate(MOCK_AF3_TEMPLATE);
+    if (companyCode && state && product) {
+      const templateKey = `${companyCode}:${state}:${product}`;
+      setTemplate(TEMPLATES[templateKey]);
     }
-  }, [product]);
+  }, [companyCode, state, product]);
 
   return {
     template
